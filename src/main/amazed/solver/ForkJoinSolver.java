8227f5769422b62cc168bf001ee38d2b015ee03a
Elsa <guscaelsa@github.com>
2020-10-15 13:33:49 +0200

    initial

---
+++ src/main/amazed/solver/ForkJoinSolver.java
@@ -0,0 +1,58 @@
+package amazed.solver;
+
+import amazed.maze.Maze;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
+
+/**
+ * <code>ForkJoinSolver</code> implements a solver for
+ * <code>Maze</code> objects using a fork/join multi-thread
+ * depth-first search.
+ * <p>
+ * Instances of <code>ForkJoinSolver</code> should be run by a
+ * <code>ForkJoinPool</code> object.
+ */
+
+
+public class ForkJoinSolver
+    extends SequentialSolver
+{
+    /**
+     * Creates a solver that searches in <code>maze</code> from the
+     * start node to a goal.
+     */
+    public ForkJoinSolver(Maze maze) {
+        super(maze);
+    }
+
+    /**
+     * Creates a solver that searches in <code>maze</code> from the
+     * start node to a goal, forking after <code>forkAfter</code> visited
+     * nodes.
+     */
+    public ForkJoinSolver(Maze maze, int forkAfter) {
+        this(maze);
+        this.forkAfter = forkAfter;
+    }
+
+    /**
+     * Searches for and returns the path, as a list of node
+     * identifiers, that goes from the start node to a goal node in
+     * the maze. If such a path cannot be found (because there are no
+     * goals, or all goals are unreacheable), the method returns
+     * <code>null</code>.
+     */
+    @Override
+    public List<Integer> compute() {
+        return parallelSearch();
+    }
+
+    private List<Integer> parallelSearch() {
+        return null;
+    }
+}
