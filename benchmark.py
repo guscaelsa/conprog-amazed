e11fd80c1ff8d6d6253f24659c21efa349ae4963
Elsa <guscaelsa@github.com>
2020-10-18 18:29:14 +0200

    the benchmarking command will be different on a different computer...

--- benchmark.py
+++ benchmark.py
@@ -1,9 +1,11 @@
 import subprocess
 
+command = "/home/tom/ownCloud/uni/java/jdk-13.0.1/bin/java -javaagent:/home/tom/ownCloud/uni/java/idea-IC-192.7142.36/lib/idea_rt.jar=35971:/home/tom/ownCloud/uni/java/idea-IC-192.7142.36/bin -Dfile.encoding=UTF-8 -classpath /home/tom/uniproj/conprog/amazed_lab/out/production/amazed_lab amazed.Main"
+
 
 def exec_command(map_file, parallelism):
     method = "sequential" if parallelism is None else "parallel-" + str(parallelism)
-    return "/home/tom/ownCloud/uni/java/jdk-13.0.1/bin/java -javaagent:/home/tom/ownCloud/uni/java/idea-IC-192.7142.36/lib/idea_rt.jar=35971:/home/tom/ownCloud/uni/java/idea-IC-192.7142.36/bin -Dfile.encoding=UTF-8 -classpath /home/tom/uniproj/conprog/amazed_lab/out/production/amazed_lab amazed.Main {} {} -1".format(map_file, method)
+    return command + " {} {} -1".format(map_file, method)
 
 
 def time_for(map_name, parallelism):
@@ -31,7 +33,7 @@ def time_for(map_name, parallelism):
         raise
 
 
-def average(map_name, parallelism, runs):
+def average(map_name, parallelism, runs=5):
     total = 0
     for i in range(runs):
         n = time_for(map_name, parallelism)
@@ -44,5 +46,5 @@ MAPS = ("small", "169medium", "medium", "custom", "large")
 if __name__ == '__main__':
     for parallelism in (None, 0, 1, 2, 3, 4, 5, 7, 10, 15, 20):
         print('| %s | ' % parallelism +
-              ' | '.join(str(average(name, parallelism, 10)) for name in MAPS) +
+              ' | '.join(str(average(name, parallelism)) for name in MAPS) +
               ' |')
